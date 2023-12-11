bc8e539a325618c5f772fcff072ef6c0a3569ca7
Amoah Ofori Darkwah <oforidarkwah7@gmail.com>
2023-12-11 22:00:28 +0000

    Added Routes for the main profile page.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import Header from './components/Header';
 import logo from './assets/logo.svg'
 import Fetch from './components/Fetch';
 import Profile from './components/Profile';
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   const userName = 'Ofori01'
   return (
     <div className="App">
     <Header  logo={logo}/>
-    <Fetch url= {`https://api.github.com/users/${userName}`} renderSuccess={Profile} />
+    <Routes>
+      <Route element={<Fetch url= {`https://api.github.com/users/${userName}`} renderSuccess={Profile} />} path='/' />
+      
+    </Routes>
       
     </div>
   );
